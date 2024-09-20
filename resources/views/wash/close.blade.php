71233e74fc2b13dab4d2a9ce0e69f5d0081f51c4
Tim <lintaitin@gmail.com>
2024-09-20 14:47:27 +0800

    20240920

--- resources/views/wash/close.blade.php
+++ resources/views/wash/close.blade.php
@@ -5,7 +5,7 @@
     <title>快點洗</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1,maximum-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css?t=20240920') }}">
 
     <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>
 
