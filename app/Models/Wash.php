e582bf5c02b204dba75fa46ace8b9bb1871f1cdf
Tim <lintaitin@gmail.com>
2024-09-28 01:20:18 +0800

    車型：

--- app/Models/Wash.php
+++ app/Models/Wash.php
@@ -54,6 +54,7 @@ class Wash extends Model
                 '姓名：' . $this->name,
                 '電話：' . $this->phone,
                 '車款：' . $this->model,
+                '車型：' . carType($this->car_type),
                 '車牌：' . $this->license,
             ]);
 
