25d9e092f9f76847c027eed49817ba6514b8075e
Tim <lintaitin@gmail.com>
2024-10-11 23:31:40 +0800

    order

---
+++ database/migrations/2024_10_11_233024_add_order_to_projects.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderToProjects extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->integer('order')->default(0)->comment('排序')->after('status');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+    }
+}
