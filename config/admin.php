2887728cf3db0f05df7a59ce61376a8d69bbe81b
Tim <lintaitin@gmail.com>
2024-11-28 16:42:16 +0800

    測試環境

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<img src="/vendor/dcat-admin/images/logo.png" width="35">' . (env('APP_ENV') != 'production') ? '測試環境' : 'Wash Admin',
+    'logo' => '<img src="/vendor/dcat-admin/images/logo.png" width="35">' . ((env('APP_ENV') != 'production') ? '測試環境' : 'Wash Admin'),
 
     /*
     |--------------------------------------------------------------------------
