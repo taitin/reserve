c13e1ed58b7b7009890e112b544912b4e9608eb7
Tim <lintaitin@gmail.com>
2024-12-26 21:34:17 +0800

    getDiscountAttribute

--- app/Models/Addition.php
+++ app/Models/Addition.php
@@ -31,11 +31,18 @@ class Addition extends Model implements Sortable
         foreach (config('wash.car_types') as $type => $name) {
             if (empty($this->use_discount)) $result[$type] = null;
             else {
-                if (!empty($this->discount_price[$type])) {
-                    $result[$type] = $this->discount_price[$type];
-                } elseif (!empty($this->discount_price[$type . '_discount'])) {
-                    $result[$type] = round($this->price[$type] * $this->discount_price[$type . '_discount'] / 100);
-                } else   $result[$type] = null;
+
+                if (
+                    strtotime(date('Y-m-d')) > strtotime($this->discount_end) ||
+                    strtotime(date('Y-m-d')) < strtotime($this->discount_start)
+                ) $result[$type] = null;
+                else {
+                    if (!empty($this->discount_price[$type])) {
+                        $result[$type] = $this->discount_price[$type];
+                    } elseif (!empty($this->discount_price[$type . '_discount'])) {
+                        $result[$type] = round($this->price[$type] * $this->discount_price[$type . '_discount'] / 100);
+                    } else   $result[$type] = null;
+                }
             }
         }
         return $result;
